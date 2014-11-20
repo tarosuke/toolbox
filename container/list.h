aeeadf70296cacd5a111b7a327c379317c546b91
tarosuke <webmaster@tarosuke.net>
2014-11-21 01:02:02 +0900

    Listをチケットの条件に合わせて修正 closes #4

--- container/list.h
+++ container/list.h
@@ -7,101 +7,108 @@
 #include "../lock/key.h"
 
 
-namespace Container{
+namespace wO{
 
-	template<class T, class L=Lock::NullLock> class List : public L{
+	template<class T, int index = 0, class L=Lock::NullLock> class List : public L{
 	public:
+		//ノード：これを継承して使う。複数のListを使うときは多重継承
 		class Node{
-			friend class ITOR;
 			friend class List;
+			frient class ITOR;
 		public:
-			Node(T& o) : prev(this), next(this), origin(&o), lock(0){};
 			~Node(){ Detach(); };
-			void Add(Node& node, L& l){ //nodeのあとに追加
-				Detach();
-				lock = &l;
-				Lock::Key<L> k(*l);
-				prev = &node;
-				next = node.next;
-				node.next = this;
-			};
-			void Insert(Node& node, L& l){ //nodeの前に挿入
-				Detach();
-				lock = &l;
-				Lock::Key<L> k(*l);
-				next = &node;
-				prev = node.prev;
-				node.prev = this;
-			};
-			void Detach(){
-				if(lock){
-					Lock::Key<L> k(*lock);
-					(*next).prev = prev;
-					(*prev).next = next;
-					prev = next = this;
-					lock = 0;
-				}
-			};
+		protected:
+			Node(T& org) : prev(this), next(this), origin(&org){};
 		private:
-			Node() : prev(this), next(this), origin(0), lock(0){};
 			Node* prev;
 			Node* next;
 			T* const origin;
-			L* lock;
-			void Detach(Lock::Key<L>&){
-				if(lock){
-					(*next).prev = prev;
-					(*prev).next = next;
-					prev = next = this;
-					lock = 0;
+			Node() : prev(this), next(this), origin(0){}; //anchor用
+			void Insert(Node& n){
+				if(prev != this){
+					Detach();
 				}
+				prev = n.prev;
+				next = &n;
+				(*prev).next = n.prev = this;
+			};
+			void Attach(Node& n){
+				if(prev != this){
+					Detach();
+				}
+				next = n.next;
+				prev = &n;
+				(*next).prev = n.next = this;
+			};
+			void Detach(){
+				(*prev).next = next;
+				(*next).prev = prev;
+				prev = next = this;
+			};
+			operator T*(){
+				return origin;
 			};
 		};
+		//STLよりJavaのそれに近い反復子
 		class ITOR{
 		public:
-			ITOR(List& l) : key(l), node(l.anchor), anchor(&l.anchor){};
+			ITOR(List& l) : key(l), node(l.anchor){};
 			operator T*(){
-				return (*node).origin;
+				return *node;
 			};
 			T* operator++(){
 				node = (*node).next;
-				return (*node).origin;
+				return *node;
 			};
 			T* operator--(){
 				node = (*node).prev;
-				return (*node).origin;
+				return *node;
 			};
 		private:
 			Lock::Key<L> key;
 			Node* node;
-			Node* const anchor;
 		};
-
+		template<typename U> T* Foreach(bool (T::*handler)(U), U param){
+			for(ITOR i(*this), ++i; *i && (*i).handler(param); ++i);
+		};
+		void Foreach(void (T::*handler)()){
+			for(ITOR i(*this), ++i; *i; ++i){
+				(*i).handler();
+			}
+		}
+		template<typename U> T* Reveach(bool (T::*handler)(U), U param){
+			for(ITOR i(*this), --i; *i && (*i).handler(param); --i);
+		};
+		void Foreach(void (T::*handler)()){
+			for(ITOR i(*this), --i; *i; --i){
+				(*i).handler();
+			}
+		};
+		//普通に操作するメソッド
 		void Add(Lock::Key<L>&, Node& n){
-			n.Add(anchor, *this);
+			n.Insert(anchor);
 		};
 		void Add(Node& n){
 			Lock::Key<L> k;
 			Add(k, n);
 		};
 		void Insert(Lock::Key<L>&, Node& n){
-			n.Insert(anchor, *this);
+			n.Append(anchor);
 		};
 		void Insert(Node& n){
 			Lock::Key<L> k;
 			Insert(k, n);
 		};
-
-		bool Is(Lock::Key<L>&){
-			return anchor.next != &anchor;
+		T* Top(Lock::Key<L>&){
+			return (*anchor.next).origin;
 		};
+
 		T* Get(Lock::Key<L>& k){
-			if(!Is(k)){
-				return;
+			Node* const top(Top(k));
+			if(top != ancher){
+				(*top).Detach(k);
 			}
-			T* const t((*anchor.next).origin);
-			(*anchor.next).Detach(k);
-			return t;
+			return *top;
 		};
 
 	private:
