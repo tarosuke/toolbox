919a6888f2a4004c926f2cbb65286d69846788ab
tarosuke <webmaster@tarosuke.net>
2016-02-20 21:34:39 +0900

    スコープ限定でglEnableするGL::Enablerを追加

--- include/toolbox/gl/gl.h
+++ include/toolbox/gl/gl.h
@@ -4,3 +4,27 @@
 
 #include <GL/glew.h>
 #include <GL/gl.h>
+
+
+namespace GL{
+
+	/** ScopeEnabler
+	* RAIIでインスタンスが存在するスコープ内でのみ機能を有効にする
+	* NOTE:スコープから出ると以前の状態とは無関係にdisableされる
+	*/
+	class Enabler{
+		Enabler();
+		Enabler(const Enabler&);
+		void operator=(const Enabler&);
+	public:
+		Enabler(int function) : function(function){
+			glEnable(function);
+		};
+		~Enabler(){
+			glDisable(function);
+		};
+	private:
+		const int function;
+	};
+
+}
