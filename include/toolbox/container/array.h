d1786e9318e3e9165c7bef8ba7204120d6e2d8ad
tarosuke <tarosukenet@gmail.com>
2016-07-11 04:20:06 +0900

    名前空間が設定されていなかったので設定 #28

--- include/toolbox/container/array.h
+++ include/toolbox/container/array.h
@@ -6,31 +6,36 @@
 #include <stdlib.h>
 
 
-template<typename T, unsigned initialSize=16> class Array{
+namespace TB{
 
-public:
-	Array() : limit(initialSize), body(malloc(sizeof(T) * initialSize){
+	template<typename T, unsigned initialSize=16> class Array{
+
+	public:
+		Array() :
+			limit(initialSize),
+			body(malloc(sizeof(T) * initialSize){
+		};
+		~Array(){
+			if(body){
+				free(body);
+			}
+		};
+		T& operator[](unsigned index){
+			if(limit <= index){
+				//伸長処理
+				limit *= 2;
+				body = realloc(body, sizeof(T) * limit);
+			}
+			if(!body){
+				//確保できなかったなどの理由で無効状態
+				return dummyEntry;
+			}
+			return body[index];
+		};
+		
+	private:
+		unsigned limit;
+		T* body;
+		T dummyEntry;
 	};
-	~Array(){
-		if(body){
-			free(body);
-		}
-	};
-	T& operator[](unsigned index){
-		if(limit <= index){
-			//伸長処理
-			limit *= 2;
-			body = realloc(body, sizeof(T) * limit);
-		}
-		if(!body){
-			//確保できなかったなどの理由で無効状態
-			return dummyEntry;
-		}
-		return body[index];
-	};
-	
-private:
-	unsigned limit;
-	T* body;
-	T dummyEntry;
-};
+}
