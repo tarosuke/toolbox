e2581040540bd896255d172fa49d35ad3be72f33
tarosuke <tarosukenet@gmail.com>
2017-07-23 19:05:21 +0900

    名前空間TBを設定 refs #48

--- cyclic/cyclic.cc
+++ cyclic/cyclic.cc
@@ -5,40 +5,42 @@
 #include <unistd.h>
 #include <toolbox/cyclic/cyclic.h>
 
+namespace TB{
 
-CYCLIC::CYCLIC(long ct, long pm) :
-	prevTime(GetNanoStamp()),
-	cycleTime(ct),
-	preMargin(pm){
+	CYCLIC::CYCLIC(long ct, long pm) :
+		prevTime(GetNanoStamp()),
+		cycleTime(ct),
+		preMargin(pm){
+	}
+
+	long CYCLIC::GetNanoStamp(){
+		struct timespec ts;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		return ts.tv_nsec;
+	}
+
+	void CYCLIC::Sleep(){
+		struct timespec ts;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+	}
+
+
+
+
+	DURATION::DURATION() : prevTime(Now()){};
+
+	double DURATION::GetDuration(){
+		const double now(Now());
+		const double duration(now - prevTime);
+		prevTime = now;
+		return duration;
+	}
+
+	double DURATION::Now(){
+		struct timespec ts;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		return ts.tv_sec + 0.000000001 * ts.tv_nsec;
+	}
+
+
 }
-
-long CYCLIC::GetNanoStamp(){
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	return ts.tv_nsec;
-}
-
-void CYCLIC::Sleep(){
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-}
-
-
-
-
-DURATION::DURATION() : prevTime(Now()){};
-
-double DURATION::GetDuration(){
-	const double now(Now());
-	const double duration(now - prevTime);
-	prevTime = now;
-	return duration;
-}
-
-double DURATION::Now(){
-	struct timespec ts;
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	return ts.tv_sec + 0.000000001 * ts.tv_nsec;
-}
-
-
--- include/toolbox/cyclic/cyclic.h
+++ include/toolbox/cyclic/cyclic.h
@@ -4,31 +4,30 @@
 #ifndef _CYCLIC_
 #define _CYCLIC_
 
+namespace TB{
 
-class CYCLIC{
-	CYCLIC();
-public:
-	CYCLIC(long cycleTime, long preMargin = 0);
-	void Sleep();
-private:
-	long prevTime; //前回のタイムスタンプ
-	long cycleTime; //設定されたサイクルタイム
-	long preMargin; //次のサイクルよりちょっと前に起きるための保険
-	long GetNanoStamp();
-};
+	class CYCLIC{
+		CYCLIC();
+	public:
+		CYCLIC(long cycleTime, long preMargin = 0);
+		void Sleep();
+	private:
+		long prevTime; //前回のタイムスタンプ
+		long cycleTime; //設定されたサイクルタイム
+		long preMargin; //次のサイクルよりちょっと前に起きるための保険
+		long GetNanoStamp();
+	};
 
-class DURATION{
-public:
-	DURATION();
-	double GetDuration();
-private:
-	double prevTime;
-	double Now();
-};
+	class DURATION{
+	public:
+		DURATION();
+		double GetDuration();
+	private:
+		double prevTime;
+		double Now();
+	};
 
-
-
-
+}
 
 #endif
 
