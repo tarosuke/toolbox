ea310cc6cedf0bbc174fd557d5195427c56a986a
tarosuke <tarosukenet@gmail.com>
2017-09-18 16:38:53 +0900

    VBOクラスを作成 closes #54

---
+++ gl/vbo.cc
@@ -0,0 +1,129 @@
+/**
+ * Copyright (C) 2017 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#include <toolbox/gl/gl.h>
+#include <toolbox/gl/vbo.h>
+
+
+namespace TB{
+
+	bool VBO::SetupBuffer(
+		Init& i,
+		unsigned noi,
+		unsigned* index,
+		unsigned nov,
+		void* vertex,
+		unsigned size){
+		//頂点バッファ確保と読み込み
+		unsigned vBuff;
+		glGenBuffers(1, &vBuff);
+		glBindBuffer(GL_ARRAY_BUFFER, vBuff);
+		glBufferData(
+			GL_ARRAY_BUFFER,
+			size * nov,
+			vertex,
+			GL_STATIC_DRAW);
+
+		//インデックスバッファ確保と読み込み
+		unsigned iBuff;
+		glGenBuffers(1, &iBuff);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iBuff);
+		glBufferData(
+			GL_ARRAY_BUFFER,
+			sizeof(int) * noi,
+			index,
+			GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		i.indexBuffer = iBuff;
+		i.vertexBuffer = vBuff;
+		i.numOfVertex = noi;
+
+		return true;
+	}
+
+	VBO::VBO(const Init& i) :
+		indexBuffer(i.indexBuffer),
+		vertexBuffer(i.vertexBuffer),
+		numOfVertex(i.numOfVertex * 3){
+	}
+
+	VBO::~VBO(){
+		glDeleteBuffers(1, &indexBuffer);
+		glDeleteBuffers(1, &vertexBuffer);
+	}
+
+	void VBO::Draw(){
+		//インデックスバッファを有効化
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+		//使うバッファを有効化
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+		//頂点バッファをセットアップ
+		const unsigned stride(sizeof(V_UV));
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexPointer(3, GL_FLOAT, stride, NULL);
+		glTexCoordPointer(2, GL_FLOAT, stride, &(*(V_UV*)0).texture);
+
+		//描画
+		glDrawElements(GL_TRIANGLES, numOfVertex, GL_UNSIGNED_INT,(void*)0);
+
+		//後始末
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	}
+
+	//UV付きVBO
+	class VBO_NORMAL : public VBO{
+	public:
+		VBO_NORMAL(const Init& i) : VBO(i){};
+		void Draw() override{
+			//インデックスバッファを有効化
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+			//使うバッファを有効化
+			glEnableClientState(GL_VERTEX_ARRAY);
+			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+			glEnableClientState(GL_NORMAL_ARRAY);
+
+			//頂点バッファをセットアップ
+			const unsigned stride(sizeof(V_UV_NORMAL));
+			glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+			glVertexPointer(3, GL_FLOAT, stride, NULL);
+			glTexCoordPointer(2, GL_FLOAT, stride, &(*(V_UV_NORMAL*)0).texture);
+			glNormalPointer(GL_FLOAT, stride, &(*(V_UV_NORMAL*)0).normal);
+
+			//描画
+			glDrawElements(GL_TRIANGLES, numOfVertex, GL_UNSIGNED_INT, (void*)0);
+
+			//後始末
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glDisableClientState(GL_VERTEX_ARRAY);
+			glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+			glDisableClientState(GL_NORMAL_ARRAY);
+		};
+	};
+
+}
---
+++ include/toolbox/gl/vbo.h
@@ -0,0 +1,84 @@
+/** 頂点バッファオブジェクトとインデクスバッファオブジェクト
+ * Copyright (C) 2017 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#pragma once
+
+
+namespace TB{
+
+	class VBO{
+		VBO();
+		VBO(const VBO&);
+		void operator=(const VBO&);
+	public:
+		struct V2{
+			float u;
+			float v;
+		}__attribute__((packed));
+		struct V3{
+			float x;
+			float y;
+			float z;
+		}__attribute__((packed));
+
+		struct V_UV{
+			V3 vertex;
+			V2 texture;
+		}__attribute__((packed));
+		struct V_UV_NORMAL{
+			V3 vertex;
+			V2 texture;
+			V3 normal;
+		}__attribute__((packed));
+
+		template<typename T> VBO* New(
+			unsigned noi,
+			unsigned* index,
+			unsigned nov,
+			T* vertex){
+			Init i;
+			return SetupBuffer(i, noi, index, nov, vertex, sizeof(T)) ? new VBO(i) : 0;
+		}
+
+		virtual void Draw();
+
+		~VBO();
+
+	protected:
+		const unsigned indexBuffer;
+		const unsigned vertexBuffer;
+		const unsigned numOfVertex;
+
+		struct Init{
+			unsigned indexBuffer;
+			unsigned vertexBuffer;
+			unsigned numOfVertex;
+		};
+		VBO(const Init&);
+
+	private:
+		bool SetupBuffer(
+			Init& i,
+			unsigned numOfIndex,
+			unsigned* index,
+			unsigned unmOfVertex,
+			void* vertex,
+			unsigned size);
+	};
+
+}
