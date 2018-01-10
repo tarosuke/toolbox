2872fca28e9f1f7599dbbd7329feea74b3a040e5
tarosuke <tarosukenet@gmail.com>
2018-01-10 19:56:47 +0900

    Pathクラスを準備 refs #66

---
+++ include/toolbox/path.h
@@ -0,0 +1,38 @@
+/** Path search
+ * Copyright (C) 2018 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#pragma once
+
+#include <toolbox/string.h>
+
+
+
+namespace TB{
+
+	class Path : public String{
+		Path();
+	protected:
+		Path(const String&, const char*);
+		Path(const String&, const String&);
+		Path(const String&, const Path&);
+
+	private:
+		static String StatPath(const String&, const String&);
+	};
+
+}
---
+++ path.cc
@@ -0,0 +1,64 @@
+/** Path search
+ * Copyright (C) 2017 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <sys/stat.h>
+
+#include <toolbox/path.h>
+
+
+
+namespace TB{
+
+	/** パスを合成しながらファイルの存在をチェックしてファイルが存在したパスを返す
+	 * 以下の場合はpathのコピーを返す。
+	 * 1. pathが絶対パス
+	 * 2. カレントディレクトリにpathが示すファイルがあった
+	 * 3. ファイルが見つからなかった
+	 * でなければresPath+pathをチェックし、ファイルがあればそれを返す。
+	 */
+	String Path::StatPath(const String& resPath, const String& path){
+		if(!path.Length() || path[0] == '/'){
+			//絶対パスか空文字列
+			return path;
+		}
+
+		//カレントディレクトリを確認
+		struct stat statBuff;
+		if(!stat(path, &statBuff)){
+			//カレントディレクトリにあったのでpathをそのまま返す
+			return path;
+		}
+
+		//resPathを追加して確認
+		if(!stat(resPath + path, &statBuff)){
+			//resPath以下にあったのでそのパスを返す
+			return path;
+		}
+
+		//ファイルがなかったのでpathをそのまま返す(帰った先でエラーになる)
+		return path;
+	}
+
+	/*** 構築子
+	 */
+	Path::Path(const String& r, const char* p) : String(StatPath(r, p)){}
+	Path::Path(const String& r, const String& p) : String(StatPath(r, p)){}
+	Path::Path(const String& r, const Path& p) : String(StatPath(r, p)){}
+
+}
