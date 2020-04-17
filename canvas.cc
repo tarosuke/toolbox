148f8704c1c015291542d42d73a548f3fecdc6a1
tarosuke <tarosukenet@gmail.com>
2020-04-18 00:38:30 +0900

    新設計の描画先であるCanvasの置き場を確保

---
+++ canvas.cc
@@ -0,0 +1,67 @@
+/********************************************************************** Canvas
+ * Copyright (C) 2020 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "include/canvas.h"
+
+#include <cairo/cairo.h>
+
+
+
+namespace TB{
+
+	Canvas::Color::Color(float r, float g, float b, float a) :
+			color(
+				((static_cast<unsigned>(255.0 * r) & 255U) << 16) |
+				((static_cast<unsigned>(255.0 * g) & 255U) << 8) |
+				(static_cast<unsigned>(255.0 * b) & 255U) |
+				((static_cast<unsigned>(255.0 * a) & 255U) << 24)){}
+
+
+	Canvas::Raw::Raw(Canvas& canvas) :
+			surface(canvas.surface),
+			body((unsigned*)cairo_image_surface_get_data(surface)),
+			bpp(GetBPP(surface)),
+			stride(cairo_image_surface_get_stride(surface) / bpp),
+			width(cairo_image_surface_get_width(surface)),
+			height(cairo_image_surface_get_height(surface)){
+		cairo_surface_flush(surface);
+	}
+
+	Canvas::Raw::~Raw(){
+		cairo_surface_mark_dirty(surface);
+	}
+
+	unsigned Canvas::Raw::GetBPP(cairo_surface_t* surface){
+		switch(cairo_image_surface_get_format(surface)){
+		case CAIRO_FORMAT_ARGB32:
+		case CAIRO_FORMAT_RGB24:
+		case CAIRO_FORMAT_RGB30:
+			return 4;
+		case CAIRO_FORMAT_RGB16_565:
+			return 2;
+		case CAIRO_FORMAT_A8:
+			return 1;
+		default:
+			break;
+		}
+		return 0;
+	}
+
+
+}
---
+++ include/canvas.h
@@ -0,0 +1,71 @@
+/********************************************************************** Canvas
+ * Copyright (C) 2020 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#pragma once
+
+#include <toolbox/geometry/rect.h>
+
+
+
+extern "C"{
+	typedef struct _cairo cairo_t;
+	typedef struct _cairo_surface cairo_surface_t;
+}
+
+
+namespace TB{
+
+	class Canvas{
+	public:
+		class Color{
+		public:
+			Color(unsigned webColor=0){ color = webColor; };
+			Color(float r=0.0, float g=0.0, float b=0.0, float a=1.0);
+		private:
+			unsigned color;
+		};
+
+		//直接アクセスするためのアクセサ
+		class Raw{
+			private:
+				cairo_surface_t* const surface;
+		public:
+			Raw(Canvas&);
+			~Raw();
+			unsigned* const body;
+			const unsigned bpp;
+			const unsigned stride;
+			const unsigned width;
+			const unsigned height;
+			unsigned* operator[](unsigned y){
+				return &body[y * stride];
+			};
+		private:
+			static unsigned GetBPP(cairo_surface_t*);
+		};
+
+
+	protected:
+		cairo_surface_t* const surface;
+
+		virtual void OnUpdated(const Rect<unsigned>&){};
+
+
+	};
+
+}
