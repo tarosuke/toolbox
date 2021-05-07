53f5d88658fa471ad4f6823cccd301329467d4fe
Yuji Tateno <tarosukenet@gmail.com>
2021-05-07 18:47:09 +0900

    全体の構成を設計したので反映

--- include/toolbox/tg/tg.h
+++ include/toolbox/tg/tg.h
@@ -1,5 +1,5 @@
 /************************************************************ toolbox graphics
- * Copyright (C) 2021 tarosuke<webmaster@tarosuke.net>
+ * Copyright (C) 2020, 2021 tarosuke<webmaster@tarosuke.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,3 +17,39 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 #pragma once
+
+#include <toolbox/container/list.h>
+#include <toolbox/geometry/vector.h>
+
+
+
+namespace TG {
+
+	//何らかの物体あるいは物体グループ
+	class Object : public TB::List<Object>::Node {
+		Object(const Object&);
+		void operator=(const Object&);
+
+	public:
+		virtual ~Object(){};
+	};
+
+	//フレームバッファや画面などの描画先
+	class Scene {
+	public:
+		struct Frustum {
+			double left;
+			double right;
+			double bottom;
+			double top;
+			double near;
+			double far;
+		};
+		Scene(const Frustum&); // Frustumで初期化
+		Scene(const double[]); // 投影行列で初期化
+		virtual ~Scene(){};
+	};
+
+	class Texture {};
+
+}
