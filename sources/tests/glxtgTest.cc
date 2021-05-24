4fba59da6a77ad25d48ab3f30a81ec88758a5160
Yuji Tateno <tarosukenet@gmail.com>
2021-05-24 20:36:24 +0900

    Frustum指定のtopとbottomが逆だったのでラベルで指定するようにした

--- sources/tests/glxtgTest.cc
+++ sources/tests/glxtgTest.cc
@@ -11,8 +11,14 @@ int main() {
 	XTG::Display display;
 	assert(display);
 
-	const TG::Scene::Frustum frustum =
-		{-0.01, 0.01, 0.01, -0.01, 0.01, 100000.0};
+	const TG::Scene::Frustum frustum = {
+		left : -0.01,
+		right : 0.01,
+		top : 0.01,
+		bottom : -0.01,
+		near : 0.01,
+		far : 100000.0
+	};
 	TG::GLXScene window(640, 640, 0, frustum);
 	window.RegisterScenery(
 		TG::Scenery::New("/home/tarosuke/Pictures/sceneries/Cubic30.png"));
