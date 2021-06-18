6e5c4956c814b2c13337fc8893e92e106bc4c836
Yuji Tateno <tarosukenet@gmail.com>
2021-06-19 04:33:19 +0900

    Matrix * Vectorのテストのフレームは書いたが問題と正解は後で書く

--- sources/tests/matrix.cc
+++ sources/tests/matrix.cc
@@ -42,5 +42,14 @@ int main() {
 			(const int[]){4, 5, 6, 7, 12, 17, 22, 27, 20, 29, 38, 47});
 		Check(a * b, c);
 	}
+
+	{
+		const TB::Matrix<3, 3, int> a((const int[]){1, 2, 3, 4, 5, 6});
+		const TB::Vector<2, int> b((const int[2]){7, 8});
+		const TB::Vector<2, float> c((const int[2]){9, 10});
+		if (a * b != c) {
+			exit(-1);
+		}
+	}
 	return 0;
 }
