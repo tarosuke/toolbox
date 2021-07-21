9689796ff44de711a470daab13b02d0e6be90920
Yuji Tateno <tarosukenet@gmail.com>
2021-07-22 01:29:16 +0900

    TB::Arrayの修正漏れを投入

--- include/toolbox/container/array.h
+++ include/toolbox/container/array.h
@@ -1,4 +1,20 @@
-/******************************************************************** 汎用配列
+/** 汎用配列
+ * Copyright (C) 2016,2021 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 #pragma once
 
@@ -9,16 +25,23 @@
 
 
 
-namespace TB{
+namespace TB {
 
-	template<typename T> class Array{
-		Array(const Array&);
-		void operator=(const Array&);
+	template <typename T> class Array {
 	public:
 		Array() : elements(0), assigned(0), body(0){};
-		Array(const T* origin, unsigned elements)
-			: elements(elements), assigned(elements),
-			  body(malloc(sizeof(T) * elements)){};
+		Array(const T origin[], unsigned elements) : elements(0), assigned(0) {
+			Resize(origin.elements);
+			memcpy(body, origin, sizeof(T) * elements);
+		};
+		Array(const Array& origin) : elements(0), assigned(0) {
+			Resize(origin.elements);
+			memcpy(body, origin.body, sizeof(T) * elements);
+		};
+		void operator=(const Array& origin) {
+			Resize(origin.elements);
+			memcpy(body, origin.body, sizeof(T) * elements);
+		};
 
 		~Array(){
 			if(body){ free(body); }
@@ -28,6 +51,12 @@ namespace TB{
 				throw std::out_of_range("TB:Array");
 			}
 			return body[index];
+		};
+
+		T* GetRawBody() const { return body; };
+		void PushBack(const T& v) {
+			Resize(elements + 1);
+			body[elements] = v;
 		};
 
 		unsigned Size() { return elements * sizeof(T); };
@@ -48,11 +77,8 @@ namespace TB{
 				throw std::runtime_error("realloc failed in 'TB::Array'");
 			}
 			body = (T*)newBody;
-		};
-		T* GetRawBody() const { return body; };
-		void PushBack(const T& v) {
-			Resize(elements + 1);
-			body[elements] = v;
+			assigned = r;
+			elements = requierd;
 		};
 
 	private:
