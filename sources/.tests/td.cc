537e867a7a039d0dcb272807cafb84e9c711a99f
Yuji Tateno <mebmaster@tarosuke.net>
2021-09-23 10:21:24 +0900

    shader.ccが消えていたので修復して動作確認

--- sources/.tests/td.cc
+++ sources/.tests/td.cc
@@ -2,17 +2,19 @@
 #include <toolbox/td/vulkan/td.h>
 
 
+#define C(t, p) _binary_##t##_td_vulkan_test_frag_spv_## p
+#define A(t) C(t, start)
+#define B(t) C(t, end)
 
-extern const u32 _binary_DEBUG_td_vulkan_test_frag_spv_start[];
-extern const u32 _binary_DEBUG_td_vulkan_test_frag_spv_end[];
+
+extern const u32 A(_BUILD_TARGET_)[];
+extern const u32 B(_BUILD_TARGET_)[];
 
 int main() {
 	ManualTest;
 
 	TB::VK::XFBTD fb(200, 200, TB::TD::Frustum(0.01, 0.01, 0.01, 100000));
-	TB::VK::Shader shader(
-		_binary_DEBUG_td_vulkan_test_frag_spv_start,
-		_binary_DEBUG_td_vulkan_test_frag_spv_end);
+	TB::VK::Shader shader(A(_BUILD_TARGET_), B(_BUILD_TARGET_));
 
 
 	return 0;
