7e15d07a9f7e9c3c0b210a083c9e80ca0bbb462c
Yuji Tateno <mebmaster@tarosuke.net>
2021-09-24 21:26:00 +0900

    Posit失敗時に場所が表示されるようにした＆libstdc++の中で止まらないようにした

--- sources/.tests/td.cc
+++ sources/.tests/td.cc
@@ -1,13 +1,16 @@
 #include <toolbox/test.h>
 #include <toolbox/td/vulkan/td.h>
 
+#include <stdio.h>
+
 
 
 int main() {
 	ManualTest;
 
-	TB::VK::XFBTD fb(200, 200, TB::TD::Frustum(0.01, 0.01, 0.01, 100000));
-
+	try {
+		TB::VK::XFBTD fb(200, 200, TB::TD::Frustum(0.01, 0.01, 0.01, 100000));
+	} catch (const char* message) { puts(message); }
 
 	return 0;
 }
