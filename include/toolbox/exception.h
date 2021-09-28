b0c8972961b5fcef8de4b7a590d4d2f9dacf0021
Yuji Tateno <mebmaster@tarosuke.net>
2021-09-28 22:11:54 +0900

    Posit失敗時のメッセージにseverityが含まれていなかったので追加

--- include/toolbox/exception.h
+++ include/toolbox/exception.h
@@ -22,7 +22,7 @@
 
 
 
-#define _THROW_ARG(f, l, c) f ":" #l ":: " #c
+#define _THROW_ARG(f, l, c) f ":" #l ":: fail: Posit failed - " #c
 #define THROW_ARG(f, l, c) _THROW_ARG(f, l, c)
 #define THROW throw THROW_ARG(__FILE__, __LINE__, )
 
