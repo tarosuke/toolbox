c3264356da33f43215a39266670fc904125d88b1
Yuji Tateno <webmaster@tarosuke.net>
2023-06-14 02:30:58 +0900

    factory.hにソース整形が適用された

--- include/toolbox/factory/factory.h
+++ include/toolbox/factory/factory.h
@@ -6,7 +6,8 @@
  * 値が入ってたらそれを返してくれる
  * Newにはパラメタなしのものと、参照パラメタ一つのものがある。
  *
- * また、scoreに スコア用メソッドを設定すると全てをスキャンして最もスコアが高かった
+ * また、scoreに
+ * スコア用メソッドを設定すると全てをスキャンして最もスコアが高かった
  * もののNewが呼ばれる。同一スコアの場合は先着順(どちらが先かは規定しない)
  * なお、scoreを設定されていないものがある場合の挙動はscoreなしのものとなるので
  * スコアによる選択を使うときは全てにscoreを設定すること。
@@ -19,37 +20,36 @@
 
 
 
-//基本形
-template<class T, typename P=void> class FACTORY{
-	FACTORY();
-	FACTORY(const FACTORY&);
-	void operator=(const FACTORY&);
-public:
-	FACTORY(T* (*f)(P&), unsigned (*s)(P&) = 0) :
-		next(start),
-		factory(f),
-		score(s){
+// 基本形
+template <class T, typename P = void> struct FACTORY {
+	FACTORY() = delete;
+	FACTORY(const FACTORY&) = delete;
+	void operator=(const FACTORY&) = delete;
+
+	FACTORY(T* (*f)(P&), unsigned (*s)(P&) = 0)
+		: next(start), factory(f), score(s) {
 		start = this;
 	};
-	static T* New(P& p){
+	static T* New(P& p) {
 		FACTORY* matched(0);
 		unsigned maxScore(0);
-		for(FACTORY* f(start); f; f = (*f).next){
-			if((*f).score){
+		for (FACTORY* f(start); f; f = (*f).next) {
+			if ((*f).score) {
 				const unsigned s((*f).score(p));
-				if(maxScore < s){
+				if (maxScore < s) {
 					matched = f;
 					maxScore = s;
 				}
-			}else{
+			} else {
 				T* const t(((*f).factory)(p));
-				if(t){
+				if (t) {
 					return t;
 				}
 			}
 		}
 		return (matched && maxScore) ? (*matched).factory(p) : 0;
 	}
+
 private:
 	static FACTORY* start;
 	FACTORY* const next;
@@ -57,37 +57,36 @@ private:
 	unsigned (*const score)(P&);
 };
 
-//引数なしの場合
-template<class T> class FACTORY<T, void>{
-	FACTORY();
-	FACTORY(const FACTORY&);
-	void operator=(const FACTORY&);
-public:
-	FACTORY(T* (*f)(), unsigned (*s)() = 0) :
-		next(start),
-		factory(f),
-		score(s){
+// 引数なしの場合
+template <class T> struct FACTORY<T, void> {
+	FACTORY() = delete;
+	FACTORY(const FACTORY&) = delete;
+	void operator=(const FACTORY&) = delete;
+
+	FACTORY(T* (*f)(), unsigned (*s)() = 0)
+		: next(start), factory(f), score(s) {
 		start = this;
 	};
-	static T* New(){
+	static T* New() {
 		FACTORY* matched(0);
 		unsigned maxScore(0);
-		for(FACTORY* f(start); f; f = (*f).next){
-			if((*f).score){
+		for (FACTORY* f(start); f; f = (*f).next) {
+			if ((*f).score) {
 				const unsigned s((*f).score());
-				if(maxScore < s){
+				if (maxScore < s) {
 					matched = f;
 					maxScore = s;
 				}
-			}else{
+			} else {
 				T* const t(((*f).factory)());
-				if(t){
+				if (t) {
 					return t;
 				}
 			}
 		}
 		return (matched && maxScore) ? (*matched).factory() : 0;
 	};
+
 private:
 	static FACTORY* start;
 	FACTORY* const next;
